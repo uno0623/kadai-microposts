e2f68abac807dbd32979e348e5b3be25843c3429
satoshi.uno <taro.kirameki@techacademy.jp>
2021-07-24 07:28:33 +0000

    post

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,7 +20,10 @@ class ApplicationController < ActionController::Base
 
   def counts(user)
     @count_microposts = user.microposts.count
+    @count_followings = user.followings.count
+    @count_followers = user.followers.count
   end
+  
   
   
 end
